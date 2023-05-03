25d5c91d315b6498fe633f10ac9e961ac59f25b9
edwin caipa <edwin.caipara@cun.edu.co>
2023-05-02 23:14:52 -0500

    cambios

--- src/js/modal-product.js
+++ src/js/modal-product.js
@@ -1,14 +1,45 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("#data-modal-open"),
-    closeModalBtn: document.querySelector("#data-modal-close"),
-    modal: document.querySelector("#data-modal"),
+    openModalBtn: document.querySelector('[data-modal-ice-cream-open]'),
+    closeModalBtn: document.querySelector('[data-modal-ice-cream-close]'),
+    modal: document.querySelector('[data-modal-ice-cream]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
+
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-ice-coffee-open]'),
+    closeModalBtn: document.querySelector('[data-modal-ice-coffee-close]'),
+    modal: document.querySelector('[data-modal-ice-coffee]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-milkshakes-open]'),
+    closeModalBtn: document.querySelector('[data-modal-milkshakes-close]'),
+    modal: document.querySelector('[data-modal-milkshakes]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+})(); s
