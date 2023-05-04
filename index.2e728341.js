f1450b5e4f994b704640900eae0f47e860507b9c
EdwinAlbeiroCaipaRamirez <32907969+EdwinAlbeiroCaipaRamirez@users.noreply.github.com>
2023-05-04 16:45:41 +0000

    Deploying to gh-pages from @ EdwinAlbeiroCaipaRamirez/icecream-test@7a0c6e93dd175a13346ff7ab8825f02673ea41c9 🚀

---
+++ index.2e728341.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("#footer__modal-open"),closeModalBtn:document.querySelector("#footer__modal-close"),modal:document.querySelector("#footer__modal-map")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.2e728341.js.map
